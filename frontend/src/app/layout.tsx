5c660a9d6f88869cc0c009e87c2f8f8df352291c
b201018 <dummy@gmail.com>
2026-04-12 14:27:26 +0900

    docker commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "My Web App" };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
