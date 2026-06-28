39e968c226b73081cb602f68eced9514fe1ddc71
sugaryuya <sugaryuya0128@yahoo.co.jp>
2026-06-28 21:58:57 +0900

    Delete frontend directory

--- frontend/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>🚀 Frontend 起動確認</h1>
-      <p>Next.js + TypeScript が正常に動いています！</p>
-    </main>
-  );
-}
